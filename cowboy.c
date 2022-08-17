d60d4e8a0c89fa5929baf0722c70ad73414689f4
dilayn <97878605+dilayn@users.noreply.github.com>
2022-08-17 20:26:35 +0400

    snprintf is not needed

--- cowboy.c
+++ cowboy.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    char output[1000000] = {0};
+    //char output[1000000] = {0};
     FILE *file = fopen("cowboy.sh", "w");
     if(file == NULL)
     {
@@ -16,8 +16,15 @@ int main(void)
     fprintf(file, "#!/bin/bash\n");
     for (int i = 1; i < counter + 1; i = i + 1)
     {
-        snprintf(output, 1000000, "/usr/bin/mkvmerge --priority lower --output '/home/dilayn/mux/remux/Cowboy Bebop - e%02d.mkv' --audio-tracks 1 --subtitle-tracks 6 --language 0:ja --track-name '0:[JySzE] ITBD' --display-dimensions 0:1448x1080 --language 1:ja --track-name '1:[iKaos] 2.0 Japanese [Original Mix] [LD] - FLAC' --sub-charset 6:UTF-8 --language 6:en --track-name '6:[CBM / JySzE] English' '(' '/home/dilayn/mux/Cowboy Bebop/Cowboy Bebop - e%1$02d.mkv' ')' --track-order 0:0,0:1,0:6\n", i);
-        fprintf(file, "%s", output);
+        fprintf(file, "/usr/bin/mkvmerge --priority lower --output '/home/dilayn/mux/remux/Cowboy Bebop - e%02d.mkv' --audio-tracks 1 --subtitle-tracks 6 --language 0:ja --track-name '0:[JySzE] ITBD' --display-dimensions 0:1448x1080 --language 1:ja --track-name '1:[iKaos] 2.0 Japanese [Original Mix] [LD] - FLAC' --sub-charset 6:UTF-8 --language 6:en --track-name '6:[CBM / JySzE] English' '(' '/home/dilayn/mux/Cowboy Bebop/Cowboy Bebop - e%1$02d.mkv' ')' --track-order 0:0,0:1,0:6\n", i);
     }
     fclose(file);
 }
+
+
+
+
+
+//snprintf(output, 1000000, "/usr/bin/mkvmerge --priority lower --output '/home/dilayn/mux/remux/Cowboy Bebop - e%02d.mkv' --audio-tracks 1 --subtitle-tracks 6 --language 0:ja --track-name '0:[JySzE] ITBD' --display-dimensions 0:1448x1080 --language 1:ja --track-name '1:[iKaos] 2.0 Japanese [Original Mix] [LD] - FLAC' --sub-charset 6:UTF-8 --language 6:en --track-name '6:[CBM / JySzE] English' '(' '/home/dilayn/mux/Cowboy Bebop/Cowboy Bebop - e%1$02d.mkv' ')' --track-order 0:0,0:1,0:6\n", i);
+
+//fprintf(file, "%s", output);
