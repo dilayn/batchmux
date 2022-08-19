5faba3f8e83770b45b18e44906f60b64e06514ba
dilayn <97878605+dilayn@users.noreply.github.com>
2022-08-19 23:46:24 +0400

    final

--- general.c
+++ general.c
@@ -1,22 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cs50.h>
+#include <string.h>
+
+
+char* string_replace(char* source, size_t sourceSize, char* substring, char* with)
+{
+    char* substring_source = strstr(source, substring);
+    if (substring_source == NULL) {
+        return NULL;
+    }
+ 
+    if (sourceSize < strlen(source) + (strlen(with) - strlen(substring)) + 1) {
+        printf("Buffer size is too small\n");
+        return NULL;
+    }
+ 
+    memmove(
+        substring_source + strlen(with),
+        substring_source + strlen(substring),
+        strlen(substring_source) - strlen(substring) + 1
+    );
+ 
+    memcpy(substring_source, with, strlen(with));
+    return substring_source + strlen(with);
+}
 
 int main(void)
 {
+    //batch file
     FILE *file = fopen("batchmux.sh", "w");
     if(file == NULL)
     {
         printf("error opening file\n");
         exit(1);
     }
+
+    //integer for episode count
     int counter = get_int("Number of episodes: \n");
+
+    //mkvmerge command
     string command = get_string("Input command\n");
 
+    //bash script header
     fprintf(file, "#!/bin/bash\n");
+
+    char output[1000000] = {0};
+    char ii[3] = {0};
+    //loop function
+    
     for (int i = 1; i < counter + 1; i++)
     {
-        fprintf(file, "%s\n", command);
+        snprintf(ii, 3, "%02d", i);
+        snprintf(output, 1000000, "%s\n", command);
+        while(string_replace(output, 1000000, "XX", ii));
+        fprintf(file, "%s", output);
     }
+
+    //end
     fclose(file);
 }
