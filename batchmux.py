c034f850a8fd026d12f6ee532e7809a08ecf1882
dilayn <97878605+dilayn@users.noreply.github.com>
2022-11-01 17:30:56 +0400

    python version

---
+++ batchmux.py
@@ -0,0 +1,23 @@
+# Open the script file
+file = open("batchmux.sh", "wt")
+
+# Specify number of episodes
+i = int(input("Number of episodes: ")) + 1
+
+# Input the mkvmerge command
+command = input("Input command: ")
+
+# Bash script header
+file.write("#!/bin/bash" + "\n")
+
+# Episode count is less than 100
+if i < 101:
+    for i in range(1, i):
+        ep = str(i)
+        file.write(command.replace("XX", ep.zfill(2)) + "\n")
+
+# Episode count is more than 100
+elif i >= 101:
+    for i in range(1, i):
+        ep = str(i)
+        file.write(command.replace("XX", ep.zfill(3)) + "\n")
